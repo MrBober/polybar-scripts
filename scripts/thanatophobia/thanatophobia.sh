6e43b7713219e17a3599f5443d9d037a5a9a9046
MrBober <53260262+MrBober@users.noreply.github.com>
2022-10-08 13:03:16 +0200

    Update thanatophobia.sh

--- scripts/thanatophobia/thanatophobia.sh
+++ scripts/thanatophobia/thanatophobia.sh
@@ -2,24 +2,25 @@
 
 # SETTINGS {{{
 
-BIRTH_DATE="2004-11-22" # Birth date
-SEX="MLE" # MLE for Male, FMLE for Female, BTSX for both sexes
+BIRTH_DATE="1970-01-01" # Birth date
+SEX="BTSX" # MLE for Male, FMLE for Female, BTSX for both sexes
 # ISO 3166-1 alpha-3 country code
 # https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
-COUNTRY="POL"
+COUNTRY="GLOBAL"
+CONFIG_FILE="/tmp/thanatophobia"
 
 # }}}
 
-if [ ! -f "/tmp/tmp_thanatophobia" ] || [ "$(head -1 /tmp/tmp_thanatophobia)" != "$BIRTH_DATE $SEX $COUNTRY" ]; then 
+if [ ! -f "$CONFIG_FILE" ] || [ "$(head -1 "$CONFIG_FILE")" != "$BIRTH_DATE $SEX $COUNTRY" ]; then 
     if [ "$COUNTRY" == "GLOBAL" ]; then
         EXPECTANCY=$(curl -s "https://apps.who.int/gho/athena/api/GHO/WHOSIS_000001?filter=REGION:GLOBAL;SEX:$SEX" | grep -oP '(?<=Numeric=\").*?(?=\")' | tail -1)
     else
         EXPECTANCY=$(curl -s "https://apps.who.int/gho/athena/api/GHO/WHOSIS_000001?filter=COUNTRY:$COUNTRY;SEX:$SEX" | grep -oP '(?<=Numeric=\").*?(?=\")' | tail -1)
     fi
-    echo $BIRTH_DATE $SEX $COUNTRY > /tmp/tmp_thanatophobia
-    echo $EXPECTANCY >> /tmp/tmp_thanatophobia
+    echo $BIRTH_DATE $SEX $COUNTRY > "$CONFIG_FILE"
+    echo $EXPECTANCY >> "$CONFIG_FILE"
 else
-    EXPECTANCY=$(tail -1 /tmp/tmp_thanatophobia)
+    EXPECTANCY=$(tail -1 "$CONFIG_FILE")
 fi
 NOW=$(date -u +%s)
 BIRTH=$(date --date="$BIRTH_DATE" -u +%s)
